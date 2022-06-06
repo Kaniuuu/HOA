7563d4607db318177391eae1e60a347967480c01
Kaniuuu <106817036+Kaniuuu@users.noreply.github.com>
2022-06-06 10:17:50 +0200

    feat(search-module) Added AutoComplete
    Search bar now with dropdown menu with suggestions.

--- src/app/modules/search/resources/services/auto-complete.service.ts
+++ src/app/modules/search/resources/services/auto-complete.service.ts
@@ -1,9 +1,12 @@
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+@Injectable()
+
 export class AutoCompleteService {
-  constructor(private http: HttpClient) {
-    this.getAutoCompleteData();
-  }
+  constructor(private http: HttpClient) {}
+
   getAutoCompleteData(): Observable<string[]> {
     return this.http.get<string[]>('./assets/data.json');
   }
--- src/app/modules/search/search.component.ts
+++ src/app/modules/search/search.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
+import { AutoCompleteService } from './resources/services/auto-complete.service';
+import { map, Observable, of, startWith } from 'rxjs';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
-
 export class SearchComponent implements OnInit {
-  constructor() { }
+  searchControl = new FormControl('');
+  options: string[] = [];
+  filteredOptions = this.searchControl.valueChanges.pipe(
+    startWith(''),
+    map((val) =>
+      val
+        ? this.options.filter(
+          (v) => v.toLowerCase().indexOf(val.toLowerCase()) === 0
+        )
+        : []
+    )
+  );
+  constructor(private autoComplete: AutoCompleteService) {  }
   ngOnInit(): void {
+    this.autoComplete.getAutoCompleteData().subscribe((val) => {
+      this.options = val;
+    });
   }
 }
